9996fb30ae8b5502e1af312cd76cceeb472ad6f5
Audiosurfer <gabolad@hotmail.com>
2014-08-31 17:12:07 -0500

    fixed issues with new subjects
    Now you can add assignments to new subjects, as well as properly label
    them by subject.

--- app.js
+++ app.js
@@ -1,23 +1,22 @@
 $(document).ready(function() {
-    $('button').on('click', function() {
+    $('body').on('click', 'button', function() {
         var toDo = $(this).prev().val();
         $(this).next().append('<li>' + toDo + '</li>');
     })
-    $('input[name=assignment]').on('keydown', function(enter) {
+    $('body').on('keydown', 'input[name=assignment]', function(enter) {
         if (enter.which == 13) {
             var toDo = $(this).val();
             $(this).next().next().append('<li>' + toDo + '</li>');
         }
     })
-    $('.subjecttype').on('click','li', function() {
+    $('body').on('click','li', function() {
         $(this).toggleClass('strike');
     })
-    $('.subjecttype').on('dblclick', 'li', function() {
+    $('body').on('dblclick', 'li', function() {
         $(this).addClass('hide');
     })
       $('img').click(function() {
-        prompt("What subject is this for?");
-        var classname = $('prompt').val();
+        var classname = prompt("What subject is this for?");
         var subject = "<div class='subjecttype'><h2>Subject - " + classname + "</h2><input type='text' name='assignment' placeholder=' Add assignment'/><button>Add</button><ol></ol></div>";
         $('header').after(subject);
     })
